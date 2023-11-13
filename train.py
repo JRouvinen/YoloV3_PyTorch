4c117b687deed5b77dbadc7d25cb2933cc86ed18
JRouvinen <43759008+JRouvinen@users.noreply.github.com>
2023-11-13 21:17:51 +0200

    data_sampler merge to master - ver 0.4.1

--- train.py
+++ train.py
@@ -1133,7 +1133,7 @@ def run(args,data_config,hyp_config,ver,clearml=None):
 
 
 if __name__ == "__main__":
-    ver = "0.4.1B - data_sampler"
+    ver = "0.4.1"
     # Check folders
     check_folders()
     parser = argparse.ArgumentParser(description="Trains the YOLOv3 model.")
