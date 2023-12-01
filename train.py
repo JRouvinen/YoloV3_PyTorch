8a12acf3c1264ab67dd61527d28497a8f794e2ec
JRouvinen <43759008+JRouvinen@users.noreply.github.com>
2023-12-01 10:22:34 +0200

    fix on writer - failed to allocate bitmap - ver 0.4.5F

--- train.py
+++ train.py
@@ -1215,7 +1215,7 @@ def run(args, data_config, hyp_config, ver, clearml=None):
 
 
 if __name__ == "__main__":
-    ver = "0.4.5D"
+    ver = "0.4.5F"
     # Check folders
     check_folders()
     parser = argparse.ArgumentParser(description="Trains the YOLOv3 model.")
